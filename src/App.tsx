5a940991805d0ecda060d8ac2e5424f3ccc1fcba
Davi Silva <davi.psilva@midway.com.br>
2023-10-25 09:58:49 -0300

    tailwindcss initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
+import "./styles/global.css";
 import { Provider as ReduxProvider } from "react-redux";
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
+import { Player } from "./pages/Player";
 
 export const App = () => {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-
-      <AddTodo />
+      <Player />
     </ReduxProvider>
   );
 };
---
+++ src/pages/Player.tsx
@@ -0,0 +1,7 @@
+export const Player = () => {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <h1>Player</h1>
+    </div>
+  );
+};
